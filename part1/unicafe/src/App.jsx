9af857d22e10a72ac6d81a59e1906914324144e1
AhogeK <ahogek@gmail.com>
2022-11-21 16:49:24 +0800

    feat: exercises 1.6.-1.14.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+// a proper place to define a component
+const Statistics = ({goodNum, neutralNum, badNum}) => {
+  if (goodNum > 0 || neutralNum > 0 && badNum > 0) {
+    const all = goodNum + neutralNum + badNum
+    return (
+      <>
+        <h1>statistics</h1>
+        <table>
+          <tr>
+            <td>good</td>
+            <td>{goodNum}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutralNum}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{badNum}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{(goodNum * 1 + badNum * -1) / all}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{goodNum / all * 100}%</td>
+          </tr>
+        </table>
+      </>
+    )
+  }
+  return (
+      <>
+        <h1>statistics</h1>
+        <span>No feedback given</span>
+      </>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+      <button onClick={onClick}>
+        {text}
+      </button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>give feedback</h1>
+        <Button onClick={() => setGood(good + 1)} text={"good"}/>
+        <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"}/>
+        <Button onClick={() => setBad(bad + 1)} text={"bad"}/>
+        <Statistics goodNum={good} neutralNum={neutral} badNum={bad} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
 
